c65f5f2065114b519371b3287f66464e2fad84d0
zubair7384 <zubair.iqbal2018@gmail.com>
2020-05-26 16:14:56 +0500

    updated some ui changes

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
-import FooterLogo from "../../assets/Home/footer-logo.png";
 import Instagram from "../../assets/Home/instagram-logo.png";
 import Watsapp from "../../assets/Home/watsapp-logo.png";
 import Linkedin from "../../assets/Home/linkedin-logo.png";
@@ -13,9 +12,6 @@ export default function () {
     <div>
       <Container fluid className="footer-container">
         <Row className="footer-wrapper">
-          {/* <Col className="logo-col">
-            <img src={FooterLogo} alt="logo footer" />
-          </Col> */}
           <Col className="description-col" lg="5">
             Digital marketing is all about iterations, optimizations &
             improvements. A successful strategy is fluid and responsive to
